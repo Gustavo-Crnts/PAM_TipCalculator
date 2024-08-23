4fb7ffb03f49cbf86a15a99e49d8c30220b07598
Gustavo-Crnts <gustavo.he.fe.santos@gmail.com>
2024-08-23 20:51:01 -0300

    Fiz a interface

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetHighButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundHighButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundLowButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
