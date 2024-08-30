d897a8e608fa8d12352b5f2d8f2ef7f6a2c234e5
Gustavo-Crnts <gustavo.he.fe.santos@gmail.com>
2024-08-30 20:47:35 -0300

    Adicionei a parte do Arrendondar para cima e para baixo.

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,7 +1,12 @@
-﻿namespace TipCalculator
+﻿using System.Diagnostics;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
+        double valorTotal = 0;
+        double valorDaGorjeta = 0;
+        double valor = 0;
 
 
         public MainPage()
@@ -25,31 +30,83 @@
 
         private void RoundHighButton_Clicked(object sender, EventArgs e)
         {
+          
 
+            valorDaGorjeta = Math.Ceiling(valorDaGorjeta);
+            valorTotal = Math.Ceiling(valorTotal);
+
+            TipLabel.Text = valorDaGorjeta.ToString("C");
+            TipLabel.Text = valorTotal.ToString("C");
         }
 
         private void RoundLowButton_Clicked(object sender, EventArgs e)
         {
+            valorDaGorjeta = Math.Floor(valorDaGorjeta);
+            valorTotal = Math.Floor(valorTotal);
 
+            TipLabel.Text = valorDaGorjeta.ToString("C");
+            TipLabel.Text = valorTotal.ToString("C");
         }
 
         private void TipSlider_ValueChanged(object sender, EventArgs e)
         {
             TipPercentLabel.Text = TipSlider.Value.ToString("#") + "%";
+
+            try
+            {
+
+                //Pegar o valor inserido ex: 100
+                double Valor = Convert.ToDouble(AmountEntry.Text);
+
+                //Calcular o valor da gorjeta de acordo com a porcentagem
+                double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
+
+                //Calcular o valor total, sendo a soma do valor + a gorjeta
+                double ValorTotal = Valor + ValorDaGorjeta;
+
+                TotalLabel.Text = ValorTotal.ToString("C");
+                TipLabel.Text = ValorDaGorjeta.ToString("C");
+
+            }
+
+            catch (Exception ex)
+            {
+                
+                Debug.WriteLine(ex.ToString());
+
+            }
         }
+
+
+
+
 
         private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //Pegar o valor inserido ex: 100
-            double Valor = Convert.ToDouble(AmountEntry.Text);
-           
-            //Calcular o valor da gorjeta de acordo com a porcentagem
-            double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
-            //Calcular o valor total, sendo a soma do valor + a gorjeta
-            double ValorTotal = Valor + ValorDaGorjeta;
+            //fé em Deus
+            try
+            {
 
-            TotalLabel.Text = ValorTotal.ToString();
-            TipLabel.Text = ValorDaGorjeta.ToString();
+                //Pegar o valor inserido ex: 100
+                double Valor = Convert.ToDouble(AmountEntry.Text);
+
+                //Calcular o valor da gorjeta de acordo com a porcentagem
+                double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
+
+                //Calcular o valor total, sendo a soma do valor + a gorjeta
+                double ValorTotal = Valor + ValorDaGorjeta;
+
+                TotalLabel.Text = ValorTotal.ToString("C");
+                TipLabel.Text = ValorDaGorjeta.ToString("C");
+
+            }
+
+            catch (Exception ex)
+            {
+                
+                Debug.WriteLine(ex.ToString());
+                
+            }
         }
     }
 }
